40e1e01b114830a89265857d52363d842895086b
mxd1365 <mike.dapiran@gmail.com>
2018-10-10 21:26:40 -0400

    Camera now follows when player hits viewable area

--- Assets/Scripts/PlayerScripts/CameraFollow.cs
+++ Assets/Scripts/PlayerScripts/CameraFollow.cs
@@ -3,47 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
+    public float lerpSpeed = 1f;
+    public float borderSize = 1f;
 
-    public float camFollowDistance = 5;
-    public float moveSpeed = 10;
-    public float lerpSpeed = .125f;
-
-    Vector2 camTargetPos;
 	void LateUpdate () {
 
         float updateTime = Time.deltaTime;
+
+        float vertExtent = Camera.main.orthographicSize;
+        float horzExtent = vertExtent * Screen.width / Screen.height;
+        vertExtent -= borderSize;
+        horzExtent -= borderSize;
 
         float xDist = transform.position.x - Camera.main.transform.position.x;
         float yDist = transform.position.y - Camera.main.transform.position.y;
 
         Vector2 newCamPos = Camera.main.transform.position;
-        if (Mathf.Abs(xDist) > camFollowDistance)
+        if (Mathf.Abs(xDist) > horzExtent)
         {
-            if (xDist > 0)
-            {
-                newCamPos.x = transform.position.x + camFollowDistance;
-            }
+            if(xDist > 0)
+                newCamPos.x += xDist - horzExtent;
             else
-            {
-                newCamPos.x = transform.position.x - camFollowDistance;
-            }
+                newCamPos.x += xDist + horzExtent;
         }
 
-        if (Mathf.Abs(yDist) > camFollowDistance)
+        if (Mathf.Abs(yDist) > vertExtent)
         {
             if (yDist > 0)
-            {
-                newCamPos.y = transform.position.y + camFollowDistance;
-            }
+                newCamPos.y += yDist - vertExtent;
             else
-            {
-                newCamPos.y = transform.position.y - camFollowDistance;
-            }
+                newCamPos.y += yDist + vertExtent;
         }
 
-        Vector2 finalCamPosition = Vector2.Lerp((Vector2)Camera.main.transform.position, newCamPos, lerpSpeed * updateTime);
 
-        Camera.main.transform.position = (new Vector3(finalCamPosition.x, finalCamPosition.y,Camera.main.transform.position.z));
+
+        Camera.main.transform.position = (new Vector3(newCamPos.x, newCamPos.y,Camera.main.transform.position.z));
         
         Debug.Log("camTargetPos: " + newCamPos);
 	}
