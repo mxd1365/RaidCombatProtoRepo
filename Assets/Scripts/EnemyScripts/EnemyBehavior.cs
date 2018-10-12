4021192ac7ae252565088c1e480bc98f4a76871a
mxd1365 <mike.dapiran@gmail.com>
2018-10-11 22:12:13 -0400

    Added Enemy Spawn with Enemies
    They are blue and mean.

---
+++ Assets/Scripts/EnemyScripts/EnemyBehavior.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour {
+
+    public EnemySpawn spawn;
+    public float wanderSpeed = 10f;
+    public float wanderWalkTime = 3.0f;
+    public float wanderStopTime = 2.0f;
+    public float turnSpeed = 10f;
+    public float aggroSpeed = 2f;
+    public Color aggroColor = new Color(1, 0, 0);
+
+    
+    enum Mode
+    {
+        wanderWalk,
+        wanderStop,
+        aggro
+    }
+
+    private Mode myMode;
+    private Rigidbody2D myBody;
+    private void Start()
+    {
+        myMode = Mode.wanderWalk;
+        myBody = GetComponent<Rigidbody2D>();
+        timeInMode = wanderWalkTime;
+        myBody.rotation = Random.Range(0, 360);
+    }
+
+    private float timeInMode;
+    void FixedUpdate ()
+    {
+        float updateTime = Time.fixedDeltaTime;
+        Debug.Log("myMode: " + myMode);
+        
+        switch (myMode)
+        {
+            case Mode.wanderWalk:
+                myBody.velocity = (Vector2)transform.right * wanderSpeed;
+                myBody.angularVelocity = 0;
+                timeInMode -= updateTime;
+                if(timeInMode <= 0)
+                {
+                    myMode = Mode.wanderStop;
+                    timeInMode = wanderStopTime;
+                }
+                break;
+            case Mode.wanderStop:
+                myBody.velocity = Vector2.zero;
+                timeInMode -= updateTime;
+                if (timeInMode <= 0)
+                {
+                    myMode = Mode.wanderWalk;
+                    timeInMode = wanderWalkTime;
+                    myBody.rotation = Random.Range(0,360);
+                }
+                break;
+            case Mode.aggro:
+                break;
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/LevelScripts/EnemySpawn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour {
+
+    public GameObject enemy;
+    public float enemyTimer = 2.0f;
+    public int maxEnemies = 20;
+
+    private float timeTillSpawn = 0f;
+    private int enemyCount = 0;
+	void Update ()
+    {
+        float updateTime = Time.deltaTime;
+        timeTillSpawn += updateTime;
+	    if(timeTillSpawn > enemyTimer)
+        {
+            GameObject newEnemy = Instantiate(enemy);
+            newEnemy.transform.position = transform.position;
+            enemyCount++;
+            timeTillSpawn = 0;
+        }
+	}
+
+    void EnemyDead()
+    {
+        enemyCount--;
+    }
+}
