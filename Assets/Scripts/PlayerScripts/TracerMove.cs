03427bb8c7e38cde92281d8431aca3e218d033aa
mxd1365 <mike.dapiran@gmail.com>
2018-10-18 22:14:37 -0400

    Our gun now fires tracers

---
+++ Assets/Scripts/PlayerScripts/TracerMove.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TracerMove : MonoBehaviour {
+
+    public float velocity = 100f;
+
+	void FixedUpdate ()
+    {
+        float update = Time.fixedDeltaTime;
+        transform.position += transform.right * velocity * update;
+	}
+}
--- Assets/Scripts/WeaponScripts/ScoutRifle.cs
+++ Assets/Scripts/WeaponScripts/ScoutRifle.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ScoutRifle : MonoBehaviour, IWeapon {
 
+    public GameObject tracerObj;
     public bool automatic = false;
 
     public bool IsAutomatic { get { return automatic; } set { automatic = value; } }
@@ -13,6 +14,9 @@ public class ScoutRifle : MonoBehaviour, IWeapon {
         RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.right);
    
         Debug.DrawRay(transform.position, transform.right * 100, Color.green, .1f,false);
+        GameObject tracer = Instantiate(tracerObj);
+        tracer.transform.position = transform.position;
+        tracer.transform.rotation = transform.rotation;
         if (hit)
         {
             Debug.Log("Hit:" + hit.rigidbody.gameObject.name);
